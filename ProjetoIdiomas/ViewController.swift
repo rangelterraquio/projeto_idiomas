00217dfe0db4962efeb6ca032da1bba0e52062ea
Rangel Dias <rterraquio@gmail.com>
2020-04-13 19:49:20 -0300

    criacao do projeto

---
+++ ProjetoIdiomas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjetoIdiomas
+//
+//  Created by Rangel Cardoso Dias on 13/04/20.
+//  Copyright © 2020 Rangel Cardoso Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
